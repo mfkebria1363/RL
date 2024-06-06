2003a9bffc8238343fde220fd9cdb6fabbafecc8
mfkebria1363 <mfkebria1363@gmail.com>
2024-06-06 03:56:28 -0700

    Insert in database

--- src/Modules/dbm/dbm.controller.ts
+++ src/Modules/dbm/dbm.controller.ts
@@ -1,4 +1,4 @@
 import { Controller } from '@nestjs/common';
 
-@Controller('dbm')
+@Controller()
 export class DbmController {}
--- src/Modules/dbm/user/user.service.ts
+++ src/Modules/dbm/user/user.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from "typeorm";
+import { response } from 'express';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectRepository(User) protected readonly userReopsitory: Repository<User> 
+    ){}
+
+
+    newUser(body: any){
+        return this.userReopsitory.save(body) 
+    }
+}
+
