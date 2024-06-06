33fb32a98791fe85a5585d9a27a2287200b0fec3
mfkebria1363 <mfkebria1363@gmail.com>
2024-06-06 07:53:12 -0700

    delete user

--- src/Modules/dbm/user/user.controller.ts
+++ src/Modules/dbm/user/user.controller.ts
@@ -1,13 +1,13 @@
-import { BadRequestException, Body, Controller, InternalServerErrorException, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, InternalServerErrorException, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { response } from 'express';
+import { NotFoundError } from 'rxjs';
 
 @Controller('user')
 export class UserController {
     constructor (
         private readonly userService : UserService
     ){}
-
 
     @Post('register')
     async register(@Body() body: any){
@@ -27,8 +27,47 @@ export class UserController {
                 message: "New user has been inserted successfully."
             }
         } catch (error) {
-            console.log(error)
             throw new InternalServerErrorException('Failed to inset new user', error);
         }
+    }
+
+    @Post('edit')
+    async edit(@Body() body: any){
+        try{
+
+            await this.userService.editUser({
+                email: body.email,
+                first_name : body.first_name,
+                last_name : body.last_name
+            })
+
+            return {
+                message: "Your information has been editted successfully."
+            }
+
+        }catch(error){
+            let msg = "Failed to edit user information"
+            if (error.response.message){
+                msg = error.response.message
+            }
+            throw new InternalServerErrorException(msg, error)
+        }
+    }
+
+    @Delete('delete')
+    async delete(@Body() body: any){
+        
+        try{
+            await this.userService.deleteUser(body.email)
+            return { message: "User has beed deleted successfully."}
+        }catch(error){
+            let msg = "Failed to edit user information"
+            if (error.response.message){
+                msg = error.response.message
+            }
+            throw new InternalServerErrorException(msg, error)
+        }
+        
+        
     }
 }
--- src/Modules/dbm/user/user.service.ts
+++ src/Modules/dbm/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from "typeorm";
@@ -13,6 +13,27 @@ export class UserService {
 
     newUser(body: any){
         return this.userReopsitory.save(body) 
+    }
+
+    async editUser(body: any){
+        const user = await this.userReopsitory.findOneBy({ email: body.email });
+        if (!user){
+            throw new NotFoundException(`User ${body.email} not found.`)
+        }
+
+        user.first_name = body.first_name
+        user.last_name = body.last_name
+        
+        return this.userReopsitory.save(user);
+
+    }
+
+    async deleteUser(email: string){
+        const res = await this.userReopsitory.delete({email})
+        if (res.affected === 0){
+            throw new NotFoundException(`Unable to delete user ${email} `)
+        }
+        return res
     }
 }
 
